ebc6365fa380c5358d42673c29145957f473507a
princelawrence388-arch <princelawrence388@gmail.com>
2026-09-15 01:39:10 +0100

    Initialize Express server with static file serving

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Shop2Game running on port ${PORT}`);
+});
